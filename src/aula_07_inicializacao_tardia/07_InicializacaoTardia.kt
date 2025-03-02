3857958a706cde4c70152f8b8400deed0382bc39
ManoelPradoMark22 <manoelprado.aecjr@gmail.com>
2025-03-01 22:27:06 -0300

    nulos

---
+++ src/aula_07_inicializacao_tardia/07_InicializacaoTardia.kt
@@ -0,0 +1,33 @@
+package aula_07_inicializacao_tardia
+
+import kotlin.properties.Delegates
+
+fun main() {
+
+     //lateinit
+
+    lateinit var a: String
+
+    //.....
+
+    //println(a)//vc só pode usar ela ao atribuir o valor, se não da erro!!!
+
+    a = "Kotlin lateinit"
+
+    println("a = $a")
+
+    //-------------------
+    //lazy - inicializacao é feita na primeira vez que ela é utilizada!
+
+    val b: String by lazy {
+        //....
+        "Kotlin lazy"
+    }
+    println("b = $b")
+
+
+    //lateinit var c: Int //nao pode dessa forma para tipos primitivos!
+    var c: Int by Delegates.notNull() // dessa forma funciona
+    c = 10
+    println("lateinit with Int - var c: Int by Delegates.notNull() / c = $c")
+}
