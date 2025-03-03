f17b0f99245524bbacbc5f8d49eff7007efa4faf
ManoelPradoMark22 <manoelprado.aecjr@gmail.com>
2025-03-03 14:17:38 -0300

    classes aninhadas

---
+++ src/aula_12_classes/12_ClassesAninhadasInterna.kt
@@ -0,0 +1,37 @@
+package aula_12_classes
+
+//class interna tem que ser uma classe dentro de uma classe - classe aninhada
+//vantagem! podemos usar os métodos da classe superior dentro da classe interna!!!!
+
+class Car2(val model: String, val year: Int) {
+
+    inner class Engine {
+
+        fun start() {
+            println("$model ($year)")
+            println("Ligando...")
+        }
+
+        inner class Fuel(val typeName: String) {
+
+            fun fuelCar() {
+                println("Abastecendo o carro '$model' com '$typeName'")
+            }
+
+        }
+
+    }
+
+}
+
+fun main() {
+
+    //mas note que, para tanto, precisamos primeiro instanciar a classe superior, para entao instanciar a interna
+    val car = Car2(model = "Chevrolet Onix", year = 2021)
+    val carEngine = car.Engine()
+    val carEngineFuelType = carEngine.Fuel(typeName = "Gasolina")
+
+    carEngineFuelType.fuelCar()
+    carEngine.start()
+
+}
